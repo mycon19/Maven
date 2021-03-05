799ef177e40665cb567fa01b62a9860e4538c9e3
myCon Studios <53592289+mycon19@users.noreply.github.com>
2021-03-04 22:31:35 -0800

    Add files via upload

---
+++ src/test/java/mycon/tc/AppTest.java
@@ -0,0 +1,24 @@
+package mycon.tc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        assertTrue(true);
+    }
+}
